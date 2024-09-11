21fd2fb258f1973b4229c74398b3f35fa31668d1
ysphotodev <ysphotodev@gmail.com>
2024-09-12 02:22:51 +0300

    Feature #1 branch is nextfeauture

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import MyImage from '../components/myImage';
 
 export default function Home() {
   return (
     <div>
       <div>home</div>
+      <div className="grid grid-cols-4 gap-10">
+        <MyImage src="/assets/1.jpg" alt="" />
+        <MyImage src="/assets/1.jpg" alt="" />
+      </div>
     </div>
   );
 }
